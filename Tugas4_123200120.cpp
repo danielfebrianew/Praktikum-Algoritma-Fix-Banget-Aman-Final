c6b8d9582faabc482cb0e6cb6e532f5999863135
Daniel Febrian Eka Wijaya <danielfebrian61@gmail.com>
2020-11-19 06:54:27 +0700

    Tugas4Daniel

--- Tugas4_123200120.cpp
+++ Tugas4_123200120.cpp
@@ -1,73 +1,68 @@
 #include <iostream> 
 using namespace std;
 
-int perkalian(int angka1, int angka2){
-	int hasil_kali = angka2;
-	for (int a = 1; a < angka1;a++){
-		hasil_kali = hasil_kali + angka2;
-	}
-	return hasil_kali;
-}
-
-int perpangkatan(int angka1, int angka2){
-	int hasil_pangkat = angka1;
-	for (int a = 1; a < angka2;a++){
-		hasil_pangkat = hasil_pangkat * angka1;
-	}
-	return hasil_pangkat;
-}
-
 int main(){
 	int opsi,angka1,angka2;
 	int a = 1;
+	char pilihan;
 
-	cout << "Perkalian dan Perpangkatan=====" << endl;
-	cout << "1. Perkalian" << endl;
-	cout << "2. Perpangkatan" << endl;
+	do {
+		cout << "\nPerkalian dan Perpangkatan=====" << endl;
+		cout << "1. Perkalian" << endl;
+		cout << "2. Perpangkatan" << endl;
 
-	cout << "\nPilih :"; cin >> opsi;
+		cout << "\nPilih :"; cin >> opsi;
 
-	switch (opsi){
-		case 1: 
-			cout << "\nInput Angka Pertama\t: "; cin >> angka1;
-			cout << "Input Angka Kedua\t: "; cin >> angka2;
+		switch (opsi){
+			case 1: 
+				cout << "\nInput Angka Pertama\t: "; cin >> angka1;
+				cout << "Input Angka Kedua\t: "; cin >> angka2;
 
-			cout << "\nHasil " << angka1 << " x " << angka2 << " adalah : " << endl;
+				cout << "\nHasil " << angka1 << " x " << angka2 << " adalah : " << endl;
 
-			do{
-				cout << angka2;
-				a++;
-					if (a <= angka1){
-						cout << " + ";}
-					else { 
-						cout << " = ";
-						cout << perkalian (angka1,angka2) << endl;}
-			}while (a <= angka1);
-			cin.get();
-		break;
+				do{
+					cout << angka2;
+					a++;
+						if (a <= angka1){
+							cout << " + ";}
+						else { 
+							cout << " = ";
+							int hasil_kali = angka2;
+							for (int a = 1; a < angka1;a++){
+								hasil_kali = hasil_kali + angka2;}
+						cout << hasil_kali << endl;}
+				}while (a <= angka1);
+				cout << "\nUlangi Perhitungan?(Y/N): "; cin >> pilihan;
+				cin.get();
+			break;
 
-		case 2: 
-			cout << "\nInput Angka Pertama\t: "; cin >> angka1;
-			cout << "Input Angka Kedua\t: "; cin >> angka2;
+			case 2: 
+				cout << "\nInput Angka Pertama\t: "; cin >> angka1;
+				cout << "Input Angka Kedua\t: "; cin >> angka2;
 
-			cout << "\nHasil " << angka1 << "^" << angka2 << " adalah : " << endl;
+				cout << "\nHasil " << angka1 << "^" << angka2 << " adalah : " << endl;
 
-			do{
-				cout << angka1;
-				a++;
-					if (a <= angka2){
-						cout << " * ";}
-					else { 
-						cout << " = ";
-						cout << perpangkatan (angka1,angka2) << endl;}
-			}while (a <= angka2);
-			cin.get();
-		break;
+				do{
+					cout << angka1;
+					a++;
+						if (a <= angka2){
+							cout << " * ";}
+						else { 
+							cout << " = ";
+								int hasil_pangkat = angka1;
+								for (int a = 1; a < angka2;a++){
+									hasil_pangkat = hasil_pangkat * angka1;}
+						cout << hasil_pangkat << endl;}
+				}while (a <= angka2);
+				cout << "\nUlangi Perhitungan?(Y/N): "; cin >> pilihan;
+			break;
 
-		default: 
-			cout << "\nInput yang Anda Masukkan Salah!!" << endl;
-	}
-	
+			default: 
+				cout << "\nInput yang Anda Masukkan Salah!!" << endl;
+				cout << "\nUlangi Input?(Y/N): "; cin >> pilihan;
+			break;}
+	}while(pilihan == 'Y' || pilihan == 'y');
+
 	cin.get();
 
 	cout << "\nTERIMAKASIH TELAH MENGGUNAKAN APLIKASI INI!!" << endl;
