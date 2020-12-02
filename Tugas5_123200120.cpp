3aa3f3d2338f644c3aed391bc4815059fd8aa38d
Daniel Febrian Eka Wijaya <danielfebrian61@gmail.com>
2020-12-02 20:27:48 +0700

    Tugas 5

--- Tugas5_123200120.cpp
+++ Tugas5_123200120.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 
 int main(){
-	int opsi,p,l,t;
-	char pilihan;
+	int opsi,p,l,t,h;
 
-	do{
 		cout << "Gambar Bangun Datar======" << endl;
 
 		cout << "1. Kotak Bolong" << endl;
@@ -23,39 +21,38 @@ int main(){
 					for(int i = 1; i <= p; i++){
 						for(int j = 1; j <= l; j++){
 							if (i == 1 || i == p || j == 1 || j == l ){
-								cout << "#";
+								cout << "*";
 							}else {
 								cout << " ";
 							}
 						}
 						cout << endl;
 					}
-					cout << "\n\nApakah anda ingin membuat persegi lagi?(Y/N)";
-					cin >> pilihan;
 				break;
 			case 2:
 				cout << "Tinggi Segitiga : "; cin >> t;
 				cout << endl;
 					for(int i = 1; i <= t; i++){
 						for(int j = 1; j <= i; j++){
-							cout << j;
+							if(j == 1){
+								cout << i << " ";
+							}else if(j == 2){
+								h = i + (t - 1);
+								cout << h << " ";
+							}else if(j > 2){
+								h = h + (t - (j - 1));
+								cout << h << " ";								
+							}	
 						}
 					cout << endl;
 					}
-					cout << "\n\nApakah anda ingin menmbuat segitiga lagi?(Y/N)";
-					cin >> pilihan;
 				break;
 			default:
 				cout << "Periksa Kembali Pilihan Anda!!" << endl;
-				cout << "\nApakah anda ingin mencoba lagi?(Y/N)";
-				cin >> pilihan;
 				break;
 		}
-	}while(pilihan == 'y' || pilihan == 'Y');
 
 	cout << endl;
-
-	cout << "TERIMAKASIH TELAH MENGGUNAKAN APLIKASI INI!!" << endl;
 
 	system("pause");
 	return 0;
